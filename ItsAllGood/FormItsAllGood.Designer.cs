01c4c5ecdfbb3fed92f8ad706ceef9804e0e3f70
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-21 16:53:07 +0200

    Add project files.

---
+++ ItsAllGood/FormItsAllGood.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ItsAllGood
+{
+    partial class FormItsAllGood
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.Location = new System.Drawing.Point(53, 66);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(175, 59);
+            this.BtnCheck.TabIndex = 0;
+            this.BtnCheck.Text = "Check";
+            this.BtnCheck.UseVisualStyleBackColor = true;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(295, 66);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(428, 320);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormItsAllGood
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCheck);
+            this.Name = "FormItsAllGood";
+            this.Text = "Its All Good";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ ItsAllGood/FormItsAllGood.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ItsAllGood
+{
+    public partial class FormItsAllGood : Form
+    {
+        public FormItsAllGood()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            int x1, y1, x2, y2;
+
+            string[] points = new string[5];
+
+            string line = "";
+            string lineSplit;
+
+            string path = Application.StartupPath + @"\good.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            int linesCount = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 1; i <= linesCount; i++)
+            {
+                line = streamReader.ReadLine();
+
+                points = line.Split(' ');
+
+                x1 = Convert.ToInt32(points[0]);
+                y1 = Convert.ToInt32(points[1]);
+                x2 = Convert.ToInt32(points[2]);
+                y2 = Convert.ToInt32(points[3]);
+
+                CheckLines(x1, y1, x2, y2);
+
+            }
+        }
+
+        private void CheckLines(int x1, int y1, int x2, int y2)
+        {
+            if (x1 == x2 || y1 == y2)
+            {
+                TxtResult.Text += "It's all good." + Environment.NewLine;
+            }
+            else
+            {
+                TxtResult.Text += "We need to fix this." + Environment.NewLine;
+            }
+        }
+    }
+}
